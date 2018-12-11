1a180e70992c6544c65b2643d782a5c26f3f29c6
cheneyweb <457299596@qq.com>
2018-12-12 00:43:30 +0800

    持续构建

--- web/client.js
+++ web/client.js
@@ -2,10 +2,9 @@ const { LoginReq, LoginRes } = require('./grpc/user_pb.js')
 const { UserClient } = require('./grpc/user_grpc_web_pb.js')
 
 let userClient = new UserClient('http://localhost:8080')
-console.log(userClient)
 let loginReq = new LoginReq()
 loginReq.setUsername('cheney')
 userClient.login(loginReq, {}, (err, res) => {
-    console.error(err)
+    // console.error(err)
     console.log(res.getRes())
 })
