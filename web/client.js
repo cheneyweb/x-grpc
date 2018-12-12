1229c8a91122cfc22955b892eed964e4069a7969
cheneyweb <457299596@qq.com>
2018-12-12 15:39:32 +0800

    swarm部署

--- web/client.js
+++ web/client.js
@@ -1,7 +1,7 @@
 const { LoginReq, LoginRes } = require('./grpc/user_pb.js')
 const { UserClient } = require('./grpc/user_grpc_web_pb.js')
 
-let userClient = new UserClient('http://webgame.na12345.com:10000')
+let userClient = new UserClient('http://webgame.na77.org:10000')
 let loginReq = new LoginReq()
 loginReq.setUsername('cheney')
 userClient.login(loginReq, {}, (err, res) => {
