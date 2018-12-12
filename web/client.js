bddcfc85058f7b16086cbd1cd8ee0f60b5d5709f
cheneyweb <457299596@qq.com>
2018-12-12 10:39:33 +0800

    初步完成部署结构

--- web/client.js
+++ web/client.js
@@ -1,7 +1,7 @@
 const { LoginReq, LoginRes } = require('./grpc/user_pb.js')
 const { UserClient } = require('./grpc/user_grpc_web_pb.js')
 
-let userClient = new UserClient('http://localhost:8080')
+let userClient = new UserClient('http://localhost:10000')
 let loginReq = new LoginReq()
 loginReq.setUsername('cheney')
 userClient.login(loginReq, {}, (err, res) => {
