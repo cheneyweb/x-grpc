eff655d4038eaf4212bad6fa980176211c25656b
cheneyweb <457299596@qq.com>
2018-12-12 14:19:25 +0800

    swarm部署

--- web/client.js
+++ web/client.js
@@ -1,10 +1,10 @@
 const { LoginReq, LoginRes } = require('./grpc/user_pb.js')
 const { UserClient } = require('./grpc/user_grpc_web_pb.js')
 
-let userClient = new UserClient('http://localhost:10000')
+let userClient = new UserClient('http://webgame.na12345.com:10000')
 let loginReq = new LoginReq()
 loginReq.setUsername('cheney')
 userClient.login(loginReq, {}, (err, res) => {
-    // console.error(err)
+    console.error(err)
     console.log(res.getRes())
 })
