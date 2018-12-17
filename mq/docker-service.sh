658f54f3e557c98a147ece6d05c69ca15b2966b5
cheneyweb <457299596@qq.com>
2018-12-17 10:54:34 +0800

    使用Service部署mq集群

--- mq/docker-service.sh
+++ mq/docker-service.sh
@@ -1,6 +1,6 @@
 #主机地址定义
-# CONSUL_JOIN="[mq_consul]"
-CONSUL_JOIN="[ext.na77.org]"
+CONSUL_JOIN="[mq_consul]"
+# export CONSUL_JOIN="[ext.na77.org]"
 #创建amqp网络
 docker network create amqp --driver overlay
 #创建consul服务（mq_consul是发现服务的主机地址，多台主机时，retry-join直接设置多台主机的IP数组即可["192.168.2.210","192.168.2.211","192.168.2.212"]）
@@ -8,7 +8,7 @@ docker service create --name mq_consul --network amqp -p 9092:8500 -e 'CONSUL_BI
 #创建rabbitmq服务
 docker service create --name mq_rabbitmq --network amqp -p 5672:5672 -p 15672:15672 -e "AUTOCLUSTER_TYPE=consul" -e "CONSUL_HOST=$CONSUL_JOIN" -e "CONSUL_PORT=9092" -e "CONSUL_SVC=rabbitmq" -e "CONSUL_SVC_ADDR_AUTO=true" -e "AUTOCLUSTER_CLEANUP=true" -e "CLEANUP_WARN_ONLY=false" -e "RABBITMQ_ERLANG_COOKIE=secrect" cheney/rabbitmq-consul:latest
 #拓展rabbitmq集群
-# docker service scale mq_rabbitmq=2
+docker service scale mq_rabbitmq=2
 # docker service update mq_rabbitmq --replicas 2
 
 #进入容器查看IP
