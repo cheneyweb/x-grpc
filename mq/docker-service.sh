08c6d7513d174dceee71a80cad3f819bf985797c
cheneyweb <457299596@qq.com>
2018-12-17 11:46:28 +0800

    完善使用service部署mq集群

--- mq/docker-service.sh
+++ mq/docker-service.sh
@@ -4,9 +4,9 @@ docker network create amqp --driver overlay
 docker service create --name mq_consul --network amqp -p 9092:8500 -e 'CONSUL_BIND_INTERFACE=eth0' -e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true}' consul:latest agent -server -ui -client=0.0.0.0 -bootstrap-expect=1 -retry-join=mq_consul
 #创建rabbitmq服务
 docker service create --name mq_rabbitmq --network amqp -p 9093:15672 -p 5672:5672 -e "AUTOCLUSTER_TYPE=consul" -e "CONSUL_HOST=mq_consul" -e "CONSUL_PORT=8500" -e "CONSUL_SVC=rabbitmq" -e "CONSUL_SVC_ADDR_AUTO=true" -e "AUTOCLUSTER_CLEANUP=true" -e "CLEANUP_WARN_ONLY=false" -e "RABBITMQ_ERLANG_COOKIE=secrect" cheney/rabbitmq-consul:latest
-
-#拓展rabbitmq集群
-# docker service scale mq_rabbitmq=2
+#拓展consul和rabbitmq集群
+docker service scale mq_consul=2
+docker service scale mq_rabbitmq=2
 # docker service update mq_rabbitmq --replicas 2
 
 #进入容器查看IP
