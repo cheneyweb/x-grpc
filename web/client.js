e1008961ba682681784203722a7c1c6578aa6c54
cheneyweb <457299596@qq.com>
2018-12-18 14:59:57 +0800

    文档

--- web/client.js
+++ web/client.js
@@ -1,7 +1,7 @@
 const { LoginReq, LoginRes } = require('./grpc/demo/user_pb.js')
 const { UserClient } = require('./grpc/demo/user_grpc_web_pb.js')
 
-let userClient = new UserClient('http://ext.na77.org:10000')
+let userClient = new UserClient('http://localhost:10000')
 
 let loginReq = new LoginReq()
 loginReq.setUsername('cheney')
